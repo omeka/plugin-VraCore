8c16720c75d5480f5b428aaed07b877457b7fada
patrickmj <patrickmjchnm@gmail.com>
2015-12-04 14:00:41 -0500

    wrap attribute inputs in a div while experimenting with styling

--- views/admin/element-attribute-form.php
+++ views/admin/element-attribute-form.php
@@ -9,25 +9,26 @@ $ignoreAttributes =  get_option('vra-core-ignore-attributes');
     
     <fieldset class='vra-attributes' style='display: none;'>
         <?php foreach($attributeNames as $attributeName): ?>
-            <label><?php echo $attributeName; ?></label>
-    
-            <?php if (isset($vraElementObject)): ?>
-                    <?php if (is_object($vraElementObject) && isset($attributeObjects[$vraElementObject->id][$attributeName])):?>
-                        <?php $attributeObject = $attributeObjects[$vraElementObject->id][$attributeName]; ?>
+            <div class='vra-attribute'>
+                <label><?php echo $attributeName; ?></label>
+        
+                <?php if (isset($vraElementObject)): ?>
+                        <?php if (is_object($vraElementObject) && isset($attributeObjects[$vraElementObject->id][$attributeName])):?>
+                            <?php $attributeObject = $attributeObjects[$vraElementObject->id][$attributeName]; ?>
+                            <input name='<?php echo $nameBase; ?>[attrs][<?php echo $attributeObject->id; ?>][<?php echo $attributeName; ?>]' type='text' value='<?php echo $attributeObject->content; ?>' ></input>
+                        <?php else: ?>
+                            <input name='<?php echo $nameBase; ?>[attrs][new][<?php echo $attributeName; ?>]' type='text' ></input>
+                        <?php endif; ?>
+                <?php else: ?>
+        
+                    <?php if (isset($attributeObjects['display'][$attributeName])): ?>
+                        <?php $attributeObject = $attributeObjects['display'][$attributeName]; ?>
                         <input name='<?php echo $nameBase; ?>[attrs][<?php echo $attributeObject->id; ?>][<?php echo $attributeName; ?>]' type='text' value='<?php echo $attributeObject->content; ?>' ></input>
                     <?php else: ?>
                         <input name='<?php echo $nameBase; ?>[attrs][new][<?php echo $attributeName; ?>]' type='text' ></input>
                     <?php endif; ?>
-            <?php else: ?>
-    
-                <?php if (isset($attributeObjects['display'][$attributeName])): ?>
-                    <?php $attributeObject = $attributeObjects['display'][$attributeName]; ?>
-                    <input name='<?php echo $nameBase; ?>[attrs][<?php echo $attributeObject->id; ?>][<?php echo $attributeName; ?>]' type='text' value='<?php echo $attributeObject->content; ?>' ></input>
-                <?php else: ?>
-                    <input name='<?php echo $nameBase; ?>[attrs][new][<?php echo $attributeName; ?>]' type='text' ></input>
                 <?php endif; ?>
-            <?php endif; ?>
-    
+            </div>
         <?php endforeach; ?>
     </fieldset>
 </div>

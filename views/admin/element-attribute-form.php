f38ecd5f380a4b50941e892c25c21e5fb1820c89
patrickmj <patrickmjchnm@gmail.com>
2016-01-11 17:02:25 -0500

    make @circa a checkbox
    still need to fix the dates form and everything, but that's a separate issue
    
    Fix #33

--- views/admin/element-attribute-form.php
+++ views/admin/element-attribute-form.php
@@ -11,21 +11,26 @@ $ignoreAttributes =  get_option('vra-core-ignore-attributes');
         <?php foreach($attributeNames as $attributeName): ?>
             <div class='vra-attribute'>
                 <label><?php echo $attributeName; ?></label>
-        
+                <?php if ($attributeName == 'circa') {
+                        $inputType = 'checkbox';
+                      } else {
+                          $inputType = 'text';
+                      }
+                ?>
                 <?php if (isset($vraElementObject)): ?>
                         <?php if (is_object($vraElementObject) && isset($attributeObjects[$vraElementObject->id][$attributeName])):?>
                             <?php $attributeObject = $attributeObjects[$vraElementObject->id][$attributeName]; ?>
-                            <input name='<?php echo $nameBase; ?>[attrs][<?php echo $attributeObject->id; ?>][<?php echo $attributeName; ?>]' type='text' value='<?php echo $attributeObject->content; ?>' ></input>
+                            <input name='<?php echo $nameBase; ?>[attrs][<?php echo $attributeObject->id; ?>][<?php echo $attributeName; ?>]' type='<?php echo $inputType?>' value='<?php echo $attributeObject->content; ?>' ></input>
                         <?php else: ?>
-                            <input name='<?php echo $nameBase; ?>[attrs][new][<?php echo $attributeName; ?>]' type='text' ></input>
+                            <input name='<?php echo $nameBase; ?>[attrs][new][<?php echo $attributeName; ?>]' type='<?php echo $inputType?>' ></input>
                         <?php endif; ?>
                 <?php else: ?>
         
                     <?php if (isset($attributeObjects['display'][$attributeName])): ?>
                         <?php $attributeObject = $attributeObjects['display'][$attributeName]; ?>
-                        <input name='<?php echo $nameBase; ?>[attrs][<?php echo $attributeObject->id; ?>][<?php echo $attributeName; ?>]' type='text' value='<?php echo $attributeObject->content; ?>' ></input>
+                        <input name='<?php echo $nameBase; ?>[attrs][<?php echo $attributeObject->id; ?>][<?php echo $attributeName; ?>]' type='<?php echo $inputType?>' value='<?php echo $attributeObject->content; ?>' ></input>
                     <?php else: ?>
-                        <input name='<?php echo $nameBase; ?>[attrs][new][<?php echo $attributeName; ?>]' type='text' ></input>
+                        <input name='<?php echo $nameBase; ?>[attrs][new][<?php echo $attributeName; ?>]' type='<?php echo $inputType?>' ></input>
                     <?php endif; ?>
                 <?php endif; ?>
             </div>

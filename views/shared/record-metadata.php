ed393c81406b4d0d0d3c7a0919901e185215afc7
patrickmj <patrickmjchnm@gmail.com>
2016-06-27 20:36:03 -0400

    switch adding 'element' class for VRA data elements

--- views/shared/record-metadata.php
+++ views/shared/record-metadata.php
@@ -1,10 +1,17 @@
 <?php foreach ($elementsForDisplay as $setName => $setElements): ?>
+<?php
+if($setName == 'VRA Core') {
+    $class = '';
+} else {
+    $class = 'element';
+}
+?>
 <div class="element-set">
     <?php if ($showElementSetHeadings): ?>
     <h2><?php echo html_escape(__($setName)); ?></h2>
     <?php endif; ?>
     <?php foreach ($setElements as $elementName => $elementInfo): ?>
-    <div id="<?php echo text_to_id(html_escape("$setName $elementName")); ?>" class="element">
+    <div id="<?php echo text_to_id(html_escape("$setName $elementName")); ?>" class="<?php echo $class; ?>">
         <h3><?php echo html_escape(__($elementName)); ?></h3>
         <?php foreach ($elementInfo['texts'] as $text): ?>
             <div class="element-text"><?php echo $text; ?></div>
